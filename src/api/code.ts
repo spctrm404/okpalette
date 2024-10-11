eaa0e5827124c24e33e5940a940a1c5c6aeb91ef
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2024-10-11 14:19:57 +0900

    solve: async problem

--- src/api/code.ts
+++ src/api/code.ts
@@ -47,187 +47,187 @@ figma.ui.postMessage({ type: 'size', width: WIDTH, height: HEIGHT, px: PX });
 const colorSpace = figma.root.documentColorProfile;
 figma.ui.postMessage({ type: 'colorSpace', colorSpace: colorSpace });
 
-type MsgType =
-  | {
-      type: string;
-      palette: Palette;
-    }
-  | {
-      type: string;
-      apcaMatrix: ApcaMatrix;
-    };
+// type MsgType =
+//   | {
+//       type: string;
+//       palette: Palette;
+//     }
+//   | {
+//       type: string;
+//       apcaMatrix: ApcaMatrix;
+//     };
 
-figma.ui.onmessage = async (msg: MsgType) => {
-  if (msg.type === 'create-palette' && 'palette' in msg) {
-    await ensureFontLoaded();
+// figma.ui.onmessage = async (msg: MsgType) => {
+//   if (msg.type === 'create-palette' && 'palette' in msg) {
+//     await ensureFontLoaded();
 
-    //     const palette = msg.palette;
+//     //     const palette = msg.palette;
 
-    //     const nodes: SceneNode[] = [];
-    //     const paletteFrame = figma.createFrame();
-    //     paletteFrame.name = `OKP-step${palette.swatchStep}_l${quantize(
-    //       100 * palette.peakLightness,
-    //       1
-    //     )}_c${quantize(100 * palette.peakChroma, 0.1)}_h${quantize(
-    //       palette.hues.from,
-    //       1
-    //     )}-${quantize(palette.hues.to, 1)}`;
-    //     const { x: centerX, y: centerY } = figma.viewport.center;
-    //     paletteFrame.x = centerX;
-    //     paletteFrame.y = centerY;
-    //     paletteFrame.resize(
-    //       2 * PALETTE_PX +
-    //         palette.swatches.length * SWATCH_W +
-    //         (palette.swatches.length - 1) * PALETTE_GX,
-    //       2 * PALETTE_PY + SWATCH_H
-    //     );
-    //     paletteFrame.fills = [
-    //       {
-    //         type: 'SOLID',
-    //         color: { r: 1, g: 1, b: 1 },
-    //       },
-    //     ];
+//     //     const nodes: SceneNode[] = [];
+//     //     const paletteFrame = figma.createFrame();
+//     //     paletteFrame.name = `OKP-step${palette.swatchStep}_l${quantize(
+//     //       100 * palette.peakLightness,
+//     //       1
+//     //     )}_c${quantize(100 * palette.peakChroma, 0.1)}_h${quantize(
+//     //       palette.hues.from,
+//     //       1
+//     //     )}-${quantize(palette.hues.to, 1)}`;
+//     //     const { x: centerX, y: centerY } = figma.viewport.center;
+//     //     paletteFrame.x = centerX;
+//     //     paletteFrame.y = centerY;
+//     //     paletteFrame.resize(
+//     //       2 * PALETTE_PX +
+//     //         palette.swatches.length * SWATCH_W +
+//     //         (palette.swatches.length - 1) * PALETTE_GX,
+//     //       2 * PALETTE_PY + SWATCH_H
+//     //     );
+//     //     paletteFrame.fills = [
+//     //       {
+//     //         type: 'SOLID',
+//     //         color: { r: 1, g: 1, b: 1 },
+//     //       },
+//     //     ];
 
-    //     palette.swatches.forEach((aSwatch, idx) => {
-    //       const swatchFrame = figma.createFrame();
-    //       paletteFrame.appendChild(swatchFrame);
-    //       swatchFrame.name = `Swatch-${idx * palette.swatchStep}`;
-    //       swatchFrame.layoutMode = 'VERTICAL';
-    //       swatchFrame.layoutSizingHorizontal = 'FIXED';
-    //       swatchFrame.layoutSizingVertical = 'FIXED';
-    //       swatchFrame.x = PALETTE_PX + idx * (SWATCH_W + PALETTE_GX);
-    //       swatchFrame.y = PALETTE_PY;
-    //       swatchFrame.resize(SWATCH_W, SWATCH_H);
-    //       swatchFrame.fills =
-    //         colorSpace === 'DISPLAY_P3'
-    //           ? [
-    //               {
-    //                 type: 'SOLID',
-    //                 color: {
-    //                   r: aSwatch.dispP3.r,
-    //                   g: aSwatch.dispP3.g,
-    //                   b: aSwatch.dispP3.b,
-    //                 },
-    //               },
-    //             ]
-    //           : [
-    //               {
-    //                 type: 'SOLID',
-    //                 color: {
-    //                   r: aSwatch.sRgb.r,
-    //                   g: aSwatch.sRgb.g,
-    //                   b: aSwatch.sRgb.b,
-    //                 },
-    //               },
-    //             ];
+//     //     palette.swatches.forEach((aSwatch, idx) => {
+//     //       const swatchFrame = figma.createFrame();
+//     //       paletteFrame.appendChild(swatchFrame);
+//     //       swatchFrame.name = `Swatch-${idx * palette.swatchStep}`;
+//     //       swatchFrame.layoutMode = 'VERTICAL';
+//     //       swatchFrame.layoutSizingHorizontal = 'FIXED';
+//     //       swatchFrame.layoutSizingVertical = 'FIXED';
+//     //       swatchFrame.x = PALETTE_PX + idx * (SWATCH_W + PALETTE_GX);
+//     //       swatchFrame.y = PALETTE_PY;
+//     //       swatchFrame.resize(SWATCH_W, SWATCH_H);
+//     //       swatchFrame.fills =
+//     //         colorSpace === 'DISPLAY_P3'
+//     //           ? [
+//     //               {
+//     //                 type: 'SOLID',
+//     //                 color: {
+//     //                   r: aSwatch.dispP3.r,
+//     //                   g: aSwatch.dispP3.g,
+//     //                   b: aSwatch.dispP3.b,
+//     //                 },
+//     //               },
+//     //             ]
+//     //           : [
+//     //               {
+//     //                 type: 'SOLID',
+//     //                 color: {
+//     //                   r: aSwatch.sRgb.r,
+//     //                   g: aSwatch.sRgb.g,
+//     //                   b: aSwatch.sRgb.b,
+//     //                 },
+//     //               },
+//     //             ];
 
-    //       const infoFrame = figma.createFrame();
-    //       swatchFrame.appendChild(infoFrame);
-    //       infoFrame.name = 'info';
-    //       infoFrame.layoutMode = 'VERTICAL';
-    //       infoFrame.layoutSizingHorizontal = 'FILL';
-    //       infoFrame.layoutSizingVertical = 'FILL';
-    //       infoFrame.paddingTop = INFO_PY;
-    //       infoFrame.paddingBottom = INFO_PY;
-    //       infoFrame.paddingLeft = INFO_PX;
-    //       infoFrame.paddingRight = INFO_PX;
-    //       infoFrame.itemSpacing = INFO_GY;
-    //       infoFrame.fills = [];
+//     //       const infoFrame = figma.createFrame();
+//     //       swatchFrame.appendChild(infoFrame);
+//     //       infoFrame.name = 'info';
+//     //       infoFrame.layoutMode = 'VERTICAL';
+//     //       infoFrame.layoutSizingHorizontal = 'FILL';
+//     //       infoFrame.layoutSizingVertical = 'FILL';
+//     //       infoFrame.paddingTop = INFO_PY;
+//     //       infoFrame.paddingBottom = INFO_PY;
+//     //       infoFrame.paddingLeft = INFO_PX;
+//     //       infoFrame.paddingRight = INFO_PX;
+//     //       infoFrame.itemSpacing = INFO_GY;
+//     //       infoFrame.fills = [];
 
-    //       const lightnessText = figma.createText();
-    //       swatchFrame.appendChild(lightnessText);
-    //       lightnessText.name = '#';
-    //       lightnessText.fontName = { family: 'Roboto Condensed', style: 'Bold' };
-    //       lightnessText.fontSize = IDX_FONTSIZE;
-    //       lightnessText.lineHeight = { value: IDX_FONTSIZE, unit: 'PIXELS' };
-    //       lightnessText.characters = `${idx * palette.swatchStep}`;
-    //       lightnessText.layoutPositioning = 'ABSOLUTE';
-    //       lightnessText.x = SWATCH_W - lightnessText.width + IDX_OFFSET_X;
-    //       lightnessText.y = SWATCH_H - lightnessText.height + IDX_OFFSET_Y;
-    //       lightnessText.fills = [
-    //         {
-    //           type: 'SOLID',
-    //           color:
-    //             idx < palette.swatches.length / 2
-    //               ? { r: 1, g: 1, b: 1 }
-    //               : { r: 0, g: 0, b: 0 },
-    //         },
-    //       ];
+//     //       const lightnessText = figma.createText();
+//     //       swatchFrame.appendChild(lightnessText);
+//     //       lightnessText.name = '#';
+//     //       lightnessText.fontName = { family: 'Roboto Condensed', style: 'Bold' };
+//     //       lightnessText.fontSize = IDX_FONTSIZE;
+//     //       lightnessText.lineHeight = { value: IDX_FONTSIZE, unit: 'PIXELS' };
+//     //       lightnessText.characters = `${idx * palette.swatchStep}`;
+//     //       lightnessText.layoutPositioning = 'ABSOLUTE';
+//     //       lightnessText.x = SWATCH_W - lightnessText.width + IDX_OFFSET_X;
+//     //       lightnessText.y = SWATCH_H - lightnessText.height + IDX_OFFSET_Y;
+//     //       lightnessText.fills = [
+//     //         {
+//     //           type: 'SOLID',
+//     //           color:
+//     //             idx < palette.swatches.length / 2
+//     //               ? { r: 1, g: 1, b: 1 }
+//     //               : { r: 0, g: 0, b: 0 },
+//     //         },
+//     //       ];
 
-    //       const okLChText = figma.createText();
-    //       infoFrame.appendChild(okLChText);
-    //       let p3RGBText;
-    //       if (colorSpace === 'DISPLAY_P3') {
-    //         p3RGBText = figma.createText();
-    //         infoFrame.appendChild(p3RGBText);
-    //       }
-    //       const hexText = figma.createText();
-    //       infoFrame.appendChild(hexText);
-    //       const gamutText = figma.createText();
-    //       infoFrame.appendChild(gamutText);
+//     //       const okLChText = figma.createText();
+//     //       infoFrame.appendChild(okLChText);
+//     //       let p3RGBText;
+//     //       if (colorSpace === 'DISPLAY_P3') {
+//     //         p3RGBText = figma.createText();
+//     //         infoFrame.appendChild(p3RGBText);
+//     //       }
+//     //       const hexText = figma.createText();
+//     //       infoFrame.appendChild(hexText);
+//     //       const gamutText = figma.createText();
+//     //       infoFrame.appendChild(gamutText);
 
-    //       infoFrame.children.forEach((child) => {
-    //         if (child.type === 'TEXT') {
-    //           const textNode = child as TextNode;
-    //           textNode.fontName = { family: 'Roboto Mono', style: 'Medium' };
-    //           textNode.fontSize = INFO_FONTSIZE;
-    //           textNode.lineHeight = { value: INFO_LINEHEIGHT, unit: 'PIXELS' };
-    //           textNode.fills = [
-    //             {
-    //               type: 'SOLID',
-    //               color:
-    //                 idx < palette.swatches.length / 2
-    //                   ? { r: 1, g: 1, b: 1 }
-    //                   : { r: 0, g: 0, b: 0 },
-    //             },
-    //           ];
-    //         }
-    //       });
+//     //       infoFrame.children.forEach((child) => {
+//     //         if (child.type === 'TEXT') {
+//     //           const textNode = child as TextNode;
+//     //           textNode.fontName = { family: 'Roboto Mono', style: 'Medium' };
+//     //           textNode.fontSize = INFO_FONTSIZE;
+//     //           textNode.lineHeight = { value: INFO_LINEHEIGHT, unit: 'PIXELS' };
+//     //           textNode.fills = [
+//     //             {
+//     //               type: 'SOLID',
+//     //               color:
+//     //                 idx < palette.swatches.length / 2
+//     //                   ? { r: 1, g: 1, b: 1 }
+//     //                   : { r: 0, g: 0, b: 0 },
+//     //             },
+//     //           ];
+//     //         }
+//     //       });
 
-    //       okLChText.name = 'oklch';
-    //       okLChText.characters =
-    //         colorSpace === 'DISPLAY_P3'
-    //           ? `oklch(${quantize(
-    //               aSwatch.dispP3Oklch.l,
-    //               LIGHTNESS_STEP
-    //             )} ${quantize(aSwatch.dispP3Oklch.c, CHROMA_STEP)} ${quantize(
-    //               aSwatch.dispP3Oklch.h,
-    //               HUE_STEP
-    //             )})`
-    //           : `oklch(${quantize(aSwatch.sRgbOklch.l, LIGHTNESS_STEP)} ${quantize(
-    //               aSwatch.sRgbOklch.c,
-    //               CHROMA_STEP
-    //             )} ${quantize(aSwatch.sRgbOklch.h, HUE_STEP)})`;
+//     //       okLChText.name = 'oklch';
+//     //       okLChText.characters =
+//     //         colorSpace === 'DISPLAY_P3'
+//     //           ? `oklch(${quantize(
+//     //               aSwatch.dispP3Oklch.l,
+//     //               LIGHTNESS_STEP
+//     //             )} ${quantize(aSwatch.dispP3Oklch.c, CHROMA_STEP)} ${quantize(
+//     //               aSwatch.dispP3Oklch.h,
+//     //               HUE_STEP
+//     //             )})`
+//     //           : `oklch(${quantize(aSwatch.sRgbOklch.l, LIGHTNESS_STEP)} ${quantize(
+//     //               aSwatch.sRgbOklch.c,
+//     //               CHROMA_STEP
+//     //             )} ${quantize(aSwatch.sRgbOklch.h, HUE_STEP)})`;
 
-    //       if (colorSpace === 'DISPLAY_P3' && p3RGBText) {
-    //         p3RGBText.name = 'displayP3-rgb';
-    //         p3RGBText.fontName = { family: 'Roboto Mono', style: 'Medium' };
-    //         p3RGBText.characters = `color(display-p3
-    //   ${quantize(aSwatch.dispP3.r, RGB_FLOAT_PRECISION)}
-    //   ${quantize(aSwatch.dispP3.g, RGB_FLOAT_PRECISION)}
-    //   ${quantize(aSwatch.dispP3.b, RGB_FLOAT_PRECISION)}
-    // )`;
-    //       }
+//     //       if (colorSpace === 'DISPLAY_P3' && p3RGBText) {
+//     //         p3RGBText.name = 'displayP3-rgb';
+//     //         p3RGBText.fontName = { family: 'Roboto Mono', style: 'Medium' };
+//     //         p3RGBText.characters = `color(display-p3
+//     //   ${quantize(aSwatch.dispP3.r, RGB_FLOAT_PRECISION)}
+//     //   ${quantize(aSwatch.dispP3.g, RGB_FLOAT_PRECISION)}
+//     //   ${quantize(aSwatch.dispP3.b, RGB_FLOAT_PRECISION)}
+//     // )`;
+//     //       }
 
-    //       hexText.name = 'hex';
-    //       hexText.characters = `#${
-    //         colorSpace === 'DISPLAY_P3' ? aSwatch.dispP3Hex : aSwatch.sRgbHex
-    //       }`;
+//     //       hexText.name = 'hex';
+//     //       hexText.characters = `#${
+//     //         colorSpace === 'DISPLAY_P3' ? aSwatch.dispP3Hex : aSwatch.sRgbHex
+//     //       }`;
 
-    //       gamutText.name = 'gamut';
-    //       gamutText.characters = aSwatch.gamut;
-    //     });
+//     //       gamutText.name = 'gamut';
+//     //       gamutText.characters = aSwatch.gamut;
+//     //     });
 
-    //     figma.currentPage.appendChild(paletteFrame);
-    //     nodes.push(paletteFrame);
-  } else if (msg.type === 'create-matrix' && 'apcaMatrix' in msg) {
-    console.log('MATRIX');
-    await ensureFontLoaded();
-    const apcaMatrix = msg.apcaMatrix;
-    const palette = apcaMatrix.palette;
-    const matrix = apcaMatrix.matrix;
-  }
-};
+//     //     figma.currentPage.appendChild(paletteFrame);
+//     //     nodes.push(paletteFrame);
+//   } else if (msg.type === 'create-matrix' && 'apcaMatrix' in msg) {
+//     console.log('MATRIX');
+//     await ensureFontLoaded();
+//     const apcaMatrix = msg.apcaMatrix;
+//     const palette = apcaMatrix.palette;
+//     const matrix = apcaMatrix.matrix;
+//   }
+// };
 
 //     const nodes: SceneNode[] = [];
 //     const paletteFrame = figma.createFrame();
